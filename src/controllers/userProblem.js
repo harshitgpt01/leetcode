9cd278adc2af0e984f227002f0eba302dacab6f3
Harshit Gupta <harshitguptajnp74@gmail.com>
2026-01-31 15:34:19 +0530

    Initial commit

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -1,5 +1,12 @@
-
+const {getLanguageById,submitBatch}=require('../utils/problemUtility')
 
 const createProblem=async(req,res)=>{
-    
+    const {title,description,difficulty,tags,visibleTestCases,startCode,referenceSolution,problemCreator}=req.body;
+
+    try{
+        for(const{language,completeCode} of referenceSolution){
+            const languageId=getLanguageById(language);
+
+            const submissions=
 }
+    }
