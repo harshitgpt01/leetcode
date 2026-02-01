e86ae0073571f172e226db70a7f5926a59e94132
Harshit Gupta <harshitguptajnp74@gmail.com>
2026-02-01 16:49:41 +0530

    Initial commit

--- src/utils/problemUtility.js
+++ src/utils/problemUtility.js
@@ -15,7 +15,7 @@ const options = {
   method: 'POST',
   url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
   params: {
-    base64_encoded: 'true'
+    base64_encoded: 'false'
   },
   headers: {
     'x-rapidapi-key': 'ab99c6ec42mshfd636ec7c6687efp1b9043jsna684835b0591',
@@ -39,4 +39,42 @@ async function fetchData(){
 return await fetchData();
 }
 
-module.exports={getLanguageById,submitBatch}
+
+
+
+const sumitToken=async(resultToken)=>{
+  const options={
+     method: 'GET',
+  url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
+  params: {
+    tokens: resultToken.join(","),
+    base64_encoded: 'false',
+    fields: '*'
+  },
+  headers: {
+    'x-rapidapi-key': 'ab99c6ec42mshfd636ec7c6687efp1b9043jsna684835b0591',
+    'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+  }
+  };
+
+  async function fetchData(){
+    try{
+      const response=await axios.request(options);
+      return response.data;
+    }
+    catch(error){
+      console.error(error);
+    }
+  }
+
+  while(true){
+    const result=await fetchData();
+    const IsResultObtained=result.submissions.every((r)=>r.status_id>2);
+    if(IsResultObtained){
+      return result.submissions;
+    }
+    await waiting(1000);
+  }
+}
+
+module.exports={getLanguageById,submitBatch,sumitToken}
