ce51155ec39d9667142514c9d039fbb5e89cea71
Harshit Gupta <harshitguptajnp74@gmail.com>
2026-02-01 23:31:53 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,42 @@ require('dotenv').config();
 const main =  require('./config/db')
 const cookieParser =  require('cookie-parser');
 const authRouter = require('./routes/userAuth');
-
+const ProblemRouter=require('./routes/ProblemCreator0')
+const redisClient=require('./config/redis')
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/user', authRouter);
+app.use('/problem',ProblemRouter)
 
 
 
-main()
-.then(async ()=>{
-    app.listen(process.env.PORT, ()=>{
-        console.log("Server listening at port number: "+ process.env.PORT);
-    })
-})
-.catch(err=> console.log("Error Occurred: "+err));
+
+const InitalizeConnection=async()=>{
+    try{
+        await Promise.all([ main(),redisClient.connect() ]);
+        console.log("Connected to DB");
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server listening at port number: "+ process.env.PORT);
+        })}
+        catch(err){
+        console.log("Error Occurred: "+ err);
+    }
+}
+InitalizeConnection();
+
+
+
+
+
+
+
+
+
+// main()
+// .then(async ()=>{
+//     app.listen(process.env.PORT, ()=>{
+//         console.log("Server listening at port number: "+ process.env.PORT);
+//     })
+// })
+// .catch(err=> console.log("Error Occurred: "+err));
