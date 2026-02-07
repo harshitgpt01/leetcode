7e625e74de692ab2491cc83024334f5dc7ae8df2
Harshit Gupta <harshitguptajnp74@gmail.com>
2026-02-08 00:36:44 +0530

    Initial commit

--- src/routes/submit.js
+++ src/routes/submit.js
@@ -2,8 +2,11 @@ const express=require('express');
 const submitRouter=express.Router();
 const userMiddleware=require('../middleware/userMiddleware');
 const submitCode=require('../controllers/UserSubmission')
-
+const {submitCode,runcode}=require('../controllers/UserSubmission')
 
 submitRouter.post("/submit/:id",userMiddleware,submitCode);
+submitRouter.post("/runcode/:id",userMiddleware,runcode);
 
+
+module.exports=submitRouter;
   
